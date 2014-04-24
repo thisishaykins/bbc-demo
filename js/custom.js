a9a0d9d94167f8f7726826e9ad68202ed5dae206
haykins360 <akinsholasamuel@gmail.com>
2014-04-24 12:49:44 +0100

    Test Html5 templates Files
    Just testing templates files

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function(){
+	// Load PhotoSwipes (Image galleries)
+	if ($('#gallery-swiper a').length) { // Check if the targetted element exists
+		var options = {}; // Set the options to a variable
+		$('#gallery-swiper a').photoSwipe(options); // Call the initializer function
+	}
+	if ($('.gallery a').length)	{
+		var options = {};
+		$('.gallery a').photoSwipe(options);
+	}
+	if ($('.photography a').length)	{
+		var options = {};
+		$('.photography a').photoSwipe(options);
+	}
+	
+	$('.notification').append('<div class="close">X</div>');
+	$('.notification .close').click(function(){
+		$(this).parent().animate({opacity: "0"});
+		$(this).parent().slideUp();
+	});
+	
+	$('blockquote').prepend('<div class="mark left">“</div>');
+	$('blockquote').append('<div class="mark right">”</div>');
+	
+	$(window).resize();
+});
+
+
+$(window).resize(function() {
+	var mt = ($('body').height() / 2) - ($('.content').height() / 2) - 30;
+	if (mt < 8) mt = 8;
+	$('.vertical-centered').css({
+		marginTop: mt + "px"
+	});
+});
---
+++ js/swipers.js
@@ -0,0 +1,25 @@
+﻿var sliderSwiper = new Swiper('#slider-swiper',{
+	loop: true,
+	grabCursor: true,
+	calculateHeight: true,
+	autoResize: true,
+	autoplay: 5000
+});
+
+// Height fix for responsive swipers when orieantation changes
+$(window).resize(function(){
+	sliderSwiper.reInit();
+});
+
+var gallerySwiper = new Swiper('#gallery-swiper',{
+	grabCursor: true,
+	calculateHeight: true,
+	freeMode: true,
+	freeModeFluid: true,
+	slidesPerView: 'auto',
+	scrollbar: {
+		container :'#gallery-swiper-scrollbar',
+		hide: false,
+		draggable: true  
+	}
+});
